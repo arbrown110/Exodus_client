d76bdc67b6b308ce47802c44742243ec63320aa2
Angel_Brown <arbrown110@gmail.com>
2021-01-12 23:12:51 -0500

    creating action folder and files.

--- src/App.js
+++ src/App.js
@@ -5,20 +5,35 @@ import { connect} from 'react-redux'
 
 class App extends Component {
   render () {
+    const adventures = this.props.adventures.map((adventure, i) => {
+      return <li key={i}>{adventure.image_url}</li>
+    })
+    const events = this.props.events.map((event, i) => {
+      return <li key={i}>{event.title}</li>
+    }) 
+
     return (
       <div className="App">
         <header className="App-header">
           <h3>Enter (picture above or background) Also Navbar</h3>
         </header>
+        <ul>{this.props.loading? <h3>.......Wait</h3> : adventures}</ul>
+        <ul>{this.props.loading? <h3>.......Wait</h3> : events}</ul>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-//   return {
+  return {
+    adventures: state.adventureReducer.adventures,
+    loading: state.adventureReducer.loading,
+    events: state.eventReducer.events,
+    eventloading: state.eventReducer.loading
+  }
 
-//   }
- }
+}
 
 export default connect(mapStateToProps)(App);
+
+
--- src/index.js
+++ src/index.js
@@ -10,13 +10,12 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducers/rootReducer'
 
 
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
-        <App />
-    </Provider>,
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
